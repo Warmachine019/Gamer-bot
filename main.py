ee9e0eab0689cd9d03323e39f1268af027828f42
Spandan <91448111+Warmachine019@users.noreply.github.com>
2025-01-03 23:54:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://appbrewery.github.io/instant_pot/")
+driver.get("https://www.python.org/")
 
-price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
-price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
-print(f"The price is {price_dollar.text}.{price_cents.text}")
+event_times = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
+events = {}
 
-# driver.close()
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text,
+    }
+print(events)
 driver.quit()
