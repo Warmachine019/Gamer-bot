9104ec017adaf2e19d2ec22f8750115cc627217b
Spandan <91448111+Warmachine019@users.noreply.github.com>
2025-01-03 23:36:00 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+#to keep chrome open
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://appbrewery.github.io/instant_pot/")
+
+price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+print(f"The price is {price_dollar.text}.{price_cents.text}")
+
+# driver.close()
+driver.quit()
